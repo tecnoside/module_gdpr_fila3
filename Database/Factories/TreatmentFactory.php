f0fb21f0c7bfdfb7db6bf5f6f4e556ec32e4462b
Malebestia <vair81@gmail.com>
2023-11-17 16:50:26 +0100

    first

--- Database/Factories/TreatmentFactory.php
+++ Database/Factories/TreatmentFactory.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Database\Factories;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Gdpr\Models\Treatment;
 
 class TreatmentFactory extends Factory
@@ -19,8 +19,6 @@ class TreatmentFactory extends Factory
 
     /**
      * Define the model's default state.
-     *
-     * @return array
      */
     public function definition(): array
     {
