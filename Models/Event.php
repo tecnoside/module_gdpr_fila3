e8daf5aece936554110ab48150947e9bc9918011
marco76tv <marco.sottana@gmail.com>
2023-12-06 15:45:25 +0100

    Update dependencies and configuration files

--- Models/Event.php
+++ Models/Event.php
@@ -10,6 +10,34 @@ use Illuminate\Support\Facades\Crypt;
 
 use function Safe\json_encode;
 
+/**
+ * Modules\Gdpr\Models\Event
+ *
+ * @property string $id
+ * @property string|null $treatment_id
+ * @property string|null $consent_id
+ * @property string $subject_id
+ * @property string $ip
+ * @property string $action
+ * @property string $payload
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Modules\Gdpr\Models\Consent|null $consent
+ * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Event newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereAction($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereConsentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event wherePayload($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereTreatmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Event extends BaseModel
 {
     use HasUuids;
