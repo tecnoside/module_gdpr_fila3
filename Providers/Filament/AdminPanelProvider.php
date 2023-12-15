f89501c33e1bf21fd5d31128d64792c69c76c3f9
marco76tv <marco.sottana@gmail.com>
2023-12-15 12:02:56 +0100

    Refactor AdminPanelProvider

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -1,12 +1,38 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Modules\Gdpr\Providers\Filament;
 
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
+=======
+namespace Modules\Gdpr\Providers\Filament;
+
+use Filament\Pages;
+use Filament\Panel;
+use Filament\Widgets;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Http\Middleware\Authenticate;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Modules\Xot\Providers\Filament\XotBasePanelProvider;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+>>>>>>> f2b93dcd (.)
 
 class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Gdpr';
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> f2b93dcd (.)
 }
