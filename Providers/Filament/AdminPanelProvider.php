f2b93dcd21ae45817a4325fea3c601b74917c4f4
marco76tv <marco.sottana@gmail.com>
2023-12-15 10:00:18 +0100

    .

---
+++ Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Gdpr\Providers\Filament;
+
+use Filament\Pages;
+use Filament\Panel;
+use Filament\Widgets;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Http\Middleware\Authenticate;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Modules\Xot\Providers\Filament\XotBasePanelProvider;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+
+class AdminPanelProvider extends XotBasePanelProvider
+{
+    protected string $module = 'Gdpr';
+
+
+}
