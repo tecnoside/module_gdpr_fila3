b8b975138ed5afbfc2e636b05002bec18fd80ba8
marco76tv <marco.sottana@gmail.com>
2023-12-18 16:56:38 +0100

    conflict

--- Models/Treatment.php
+++ Models/Treatment.php
@@ -21,7 +21,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @property \Illuminate\Support\Carbon|null $updated_at
  *
  * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
-<<<<<<< HEAD
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   query()
@@ -35,21 +34,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereRequired($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereWeight($value)
-=======
- * @method static Builder|Treatment                                 newModelQuery()
- * @method static Builder|Treatment                                 newQuery()
- * @method static Builder|Treatment                                 query()
- * @method static Builder|Treatment                                 whereActive($value)
- * @method static Builder|Treatment                                 whereCreatedAt($value)
- * @method static Builder|Treatment                                 whereDescription($value)
- * @method static Builder|Treatment                                 whereDocumentUrl($value)
- * @method static Builder|Treatment                                 whereDocumentVersion($value)
- * @method static Builder|Treatment                                 whereId($value)
- * @method static Builder|Treatment                                 whereName($value)
- * @method static Builder|Treatment                                 whereRequired($value)
- * @method static Builder|Treatment                                 whereUpdatedAt($value)
- * @method static Builder|Treatment                                 whereWeight($value)
->>>>>>> 4a1eeab7 (Lint)
  *
  * @mixin \Eloquent
  */
