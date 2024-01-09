d23deb4872ddf68712481e746de77240b3dbbfb7
marco76tv <marco.sottana@gmail.com>
2024-01-09 12:29:39 +0100

    rebase 5

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -6,7 +6,15 @@ namespace Modules\Gdpr\Providers\Filament;
 
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
 
+<<<<<<< HEAD
 class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Gdpr';
+=======
+
+class AdminPanelProvider extends XotBasePanelProvider
+{
+    protected string $module = 'Gdpr';
+
+>>>>>>> 0ebf34c4 (conflict)
 }
