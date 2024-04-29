1afde3a8b7b83767c249140b4db98ef00f09b4af
marco76tv <marco.sottana@gmail.com>
2024-04-29 16:42:23 +0200

    .

--- Models/Consent.php
+++ Models/Consent.php
@@ -15,6 +15,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string $subject_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+<<<<<<< HEAD
  * @property Treatment|null $treatment
  *
  * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
@@ -26,6 +27,19 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @method static \Illuminate\Database\Eloquent\Builder|Consent whereSubjectId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent whereTreatmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent whereUpdatedAt($value)
+=======
+ * @property Treatment|null                  $treatment
+ *
+ * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereTreatmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
+>>>>>>> fa4ebca (Lint)
  *
  * @mixin \Eloquent
  */
--- Models/Event.php
+++ Models/Event.php
@@ -22,6 +22,7 @@ use function Safe\json_encode;
  * @property string $payload
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+<<<<<<< HEAD
  * @property Consent|null $consent
  *
  * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
@@ -37,6 +38,23 @@ use function Safe\json_encode;
  * @method static Builder|Event whereSubjectId($value)
  * @method static Builder|Event whereTreatmentId($value)
  * @method static Builder|Event whereUpdatedAt($value)
+=======
+ * @property Consent|null                    $consent
+ *
+ * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+ * @method static Builder|Event                                 newModelQuery()
+ * @method static Builder|Event                                 newQuery()
+ * @method static Builder|Event                                 query()
+ * @method static Builder|Event                                 whereAction($value)
+ * @method static Builder|Event                                 whereConsentId($value)
+ * @method static Builder|Event                                 whereCreatedAt($value)
+ * @method static Builder|Event                                 whereId($value)
+ * @method static Builder|Event                                 whereIp($value)
+ * @method static Builder|Event                                 wherePayload($value)
+ * @method static Builder|Event                                 whereSubjectId($value)
+ * @method static Builder|Event                                 whereTreatmentId($value)
+ * @method static Builder|Event                                 whereUpdatedAt($value)
+>>>>>>> fa4ebca (Lint)
  *
  * @mixin \Eloquent
  */
