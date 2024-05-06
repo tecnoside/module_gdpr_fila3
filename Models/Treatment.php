5aded54466133fc4647daf95b4bd6e09f8f1637d
marco76tv <marco76tv@users.noreply.github.com>
2024-05-06 12:39:41 +0000

    Lint

--- Models/Treatment.php
+++ Models/Treatment.php
@@ -9,31 +9,31 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 /**
  * Modules\Gdpr\Models\Treatment.
  *
- * @property string $id
- * @property int $active
- * @property int $required
- * @property string $name
- * @property string $description
- * @property string|null $documentVersion
- * @property string|null $documentUrl
- * @property int $weight
+ * @property string                          $id
+ * @property int                             $active
+ * @property int                             $required
+ * @property string                          $name
+ * @property string                          $description
+ * @property string|null                     $documentVersion
+ * @property string|null                     $documentUrl
+ * @property int                             $weight
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  *
  * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
- * @method static Builder|Treatment newModelQuery()
- * @method static Builder|Treatment newQuery()
- * @method static Builder|Treatment query()
- * @method static Builder|Treatment whereActive($value)
- * @method static Builder|Treatment whereCreatedAt($value)
- * @method static Builder|Treatment whereDescription($value)
- * @method static Builder|Treatment whereDocumentUrl($value)
- * @method static Builder|Treatment whereDocumentVersion($value)
- * @method static Builder|Treatment whereId($value)
- * @method static Builder|Treatment whereName($value)
- * @method static Builder|Treatment whereRequired($value)
- * @method static Builder|Treatment whereUpdatedAt($value)
- * @method static Builder|Treatment whereWeight($value)
+ * @method static Builder|Treatment                                 newModelQuery()
+ * @method static Builder|Treatment                                 newQuery()
+ * @method static Builder|Treatment                                 query()
+ * @method static Builder|Treatment                                 whereActive($value)
+ * @method static Builder|Treatment                                 whereCreatedAt($value)
+ * @method static Builder|Treatment                                 whereDescription($value)
+ * @method static Builder|Treatment                                 whereDocumentUrl($value)
+ * @method static Builder|Treatment                                 whereDocumentVersion($value)
+ * @method static Builder|Treatment                                 whereId($value)
+ * @method static Builder|Treatment                                 whereName($value)
+ * @method static Builder|Treatment                                 whereRequired($value)
+ * @method static Builder|Treatment                                 whereUpdatedAt($value)
+ * @method static Builder|Treatment                                 whereWeight($value)
  *
  * @mixin \Eloquent
  */
