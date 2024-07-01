dbb3a257d19fa80743a4fa77f7ed9d3662319a9b
marco <marco.sottana@gmail.com>
2024-07-01 10:54:59 +0200

    📝 (Consent.php, Event.php, Profile.php, Treatment.php): Remove unnecessary commented-out code and conflict markers to clean up the files and improve readability.

--- Models/Event.php
+++ Models/Event.php
@@ -13,49 +13,6 @@ use Illuminate\Support\Facades\Crypt;
 
 use function Safe\json_encode;
 
-/**
- * Modules\Gdpr\Models\Event.
- *
- * @property string                          $id
- * @property string|null                     $treatment_id
- * @property string|null                     $consent_id
- * @property string                          $subject_id
- * @property string                          $ip
- * @property string                          $action
- * @property string                          $payload
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property Consent|null                    $consent
- *
- * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- * @method static Builder|Event                                 newModelQuery()
- * @method static Builder|Event                                 newQuery()
- * @method static Builder|Event                                 query()
- * @method static Builder|Event                                 whereAction($value)
- * @method static Builder|Event                                 whereConsentId($value)
- * @method static Builder|Event                                 whereCreatedAt($value)
- * @method static Builder|Event                                 whereId($value)
- * @method static Builder|Event                                 whereIp($value)
- * @method static Builder|Event                                 wherePayload($value)
- * @method static Builder|Event                                 whereSubjectId($value)
- * @method static Builder|Event                                 whereTreatmentId($value)
- * @method static Builder|Event                                 whereUpdatedAt($value)
- *                                                                                                  <<<<<<< HEAD
- *                                                                                                  =======
- *
- * @property string|null                     $updated_by
- * @property string|null                     $created_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null                     $deleted_by
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedBy($value)
- *                                                                                   >>>>>>> 76348cc6b642ef946f57e308db4e1c6210448bea
- *
- * @mixin \Eloquent
- */
 class Event extends BaseModel
 {
     use HasUuids;
--- Models/Treatment.php
+++ Models/Treatment.php
@@ -6,50 +6,6 @@ namespace Modules\Gdpr\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-/**
- * Modules\Gdpr\Models\Treatment.
- *
- * @property string                          $id
- * @property int                             $active
- * @property int                             $required
- * @property string                          $name
- * @property string                          $description
- * @property string|null                     $documentVersion
- * @property string|null                     $documentUrl
- * @property int                             $weight
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- *
- * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
- * @method static Builder|Treatment                                 newModelQuery()
- * @method static Builder|Treatment                                 newQuery()
- * @method static Builder|Treatment                                 query()
- * @method static Builder|Treatment                                 whereActive($value)
- * @method static Builder|Treatment                                 whereCreatedAt($value)
- * @method static Builder|Treatment                                 whereDescription($value)
- * @method static Builder|Treatment                                 whereDocumentUrl($value)
- * @method static Builder|Treatment                                 whereDocumentVersion($value)
- * @method static Builder|Treatment                                 whereId($value)
- * @method static Builder|Treatment                                 whereName($value)
- * @method static Builder|Treatment                                 whereRequired($value)
- * @method static Builder|Treatment                                 whereUpdatedAt($value)
- * @method static Builder|Treatment                                 whereWeight($value)
- *                                                                                                      <<<<<<< HEAD
- *                                                                                                      =======
- *
- * @property string|null                     $updated_by
- * @property string|null                     $created_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null                     $deleted_by
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment whereUpdatedBy($value)
- *                                                                                       >>>>>>> 76348cc6b642ef946f57e308db4e1c6210448bea
- *
- * @mixin \Eloquent
- */
 class Treatment extends BaseModel
 {
     use HasUuids;
