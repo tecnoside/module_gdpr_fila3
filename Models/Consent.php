813f345c5cfb717517bdb7e23bfbe691c286d719
marco <marco.sottana@gmail.com>
2024-07-02 17:07:30 +0200

    📝 (Consent.php): Remove unnecessary comments and code blocks from Consent model 📝 (Event.php): Remove unnecessary comments and code blocks from Event model 📝 (Profile.php): Remove unnecessary comments and code blocks from Profile model 📝 (Treatment.php): Remove unnecessary comments and code blocks from Treatment model

--- Models/Consent.php
+++ Models/Consent.php
@@ -7,44 +7,6 @@ namespace Modules\Gdpr\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-<<<<<<< HEAD
-=======
-/**
- * Modules\Gdpr\Models\Consent.
- *
- * @property string $id
- * @property string $treatment_id
- * @property string $subject_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property Treatment|null $treatment
- *
- * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent query()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereUpdatedAt($value)
- *
- * @property Treatment|null $treatment
- *
- * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent query()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereUpdatedAt($value)
- *                                                                                     >>>>>>> fa4ebca (Lint)
- *
- * @mixin \Eloquent
- */
->>>>>>> d2f37dc (Dusting)
 class Consent extends BaseModel
 {
     use HasUuids;
