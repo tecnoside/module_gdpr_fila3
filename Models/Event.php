277fe5155fa7a34d4ce7698193188037c36517a1
marco76tv <marco76tv@users.noreply.github.com>
2024-07-02 15:13:09 +0000

    Lint

--- Models/Event.php
+++ Models/Event.php
@@ -14,39 +14,39 @@ use Illuminate\Support\Facades\Crypt;
 use function Safe\json_encode;
 
 /**
- * 
- *
- * @property string $id
- * @property string|null $treatment_id
- * @property string|null $consent_id
- * @property string $subject_id
- * @property string $ip
- * @property string $action
- * @property string $payload
+ * @property string                          $id
+ * @property string|null                     $treatment_id
+ * @property string|null                     $consent_id
+ * @property string                          $subject_id
+ * @property string                          $ip
+ * @property string                          $action
+ * @property string                          $payload
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $updated_by
- * @property string|null $created_by
+ * @property string|null                     $updated_by
+ * @property string|null                     $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null $deleted_by
- * @property-read \Modules\Gdpr\Models\Consent|null $consent
+ * @property string|null                     $deleted_by
+ * @property Consent|null                    $consent
+ *
  * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Event newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event query()
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereAction($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereConsentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereIp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event wherePayload($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereAction($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereConsentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   wherePayload($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereTreatmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event   whereUpdatedBy($value)
+ *
  * @mixin \Eloquent
  */
 class Event extends BaseModel
