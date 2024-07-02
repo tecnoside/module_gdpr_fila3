703d0bff9812d2b49c4be008e58c4d5a208097d0
marco <marco.sottana@gmail.com>
2024-07-02 17:05:22 +0200

    📝 (Consent.php): Update docblock comments for the Consent model 📝 (Event.php): Update docblock comments for the Event model 📝 (Profile.php): Update docblock comments for the Profile model 📝 (Treatment.php): Update docblock comments for the Treatment model

--- Models/Event.php
+++ Models/Event.php
@@ -25,9 +25,32 @@ use function Safe\json_encode;
  * @property string                          $payload
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+<<<<<<< HEAD
  * @property Consent|null                    $consent
  *
  * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+=======
+<<<<<<< HEAD
+ * @property Consent|null $consent
+ *
+ * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+ * @method static Builder|Event newModelQuery()
+ * @method static Builder|Event newQuery()
+ * @method static Builder|Event query()
+ * @method static Builder|Event whereAction($value)
+ * @method static Builder|Event whereConsentId($value)
+ * @method static Builder|Event whereCreatedAt($value)
+ * @method static Builder|Event whereId($value)
+ * @method static Builder|Event whereIp($value)
+ * @method static Builder|Event wherePayload($value)
+ * @method static Builder|Event whereSubjectId($value)
+ * @method static Builder|Event whereTreatmentId($value)
+ * @method static Builder|Event whereUpdatedAt($value)
+=======
+ * @property Consent|null                    $consent
+ *
+ * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+>>>>>>> f06764d (.)
  * @method static Builder|Event                                 newModelQuery()
  * @method static Builder|Event                                 newQuery()
  * @method static Builder|Event                                 query()
@@ -40,6 +63,10 @@ use function Safe\json_encode;
  * @method static Builder|Event                                 whereSubjectId($value)
  * @method static Builder|Event                                 whereTreatmentId($value)
  * @method static Builder|Event                                 whereUpdatedAt($value)
+<<<<<<< HEAD
+=======
+>>>>>>> fa4ebca (Lint)
+>>>>>>> f06764d (.)
  *
  * @mixin \Eloquent
  */
--- Models/Profile.php
+++ Models/Profile.php
@@ -5,22 +5,67 @@ declare(strict_types=1);
 namespace Modules\Gdpr\Models;
 
 /**
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+ * @property int $id
+ * @property string|null $type
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $full_name
+ * @property string|null $email
+=======
+>>>>>>> f06764d (.)
  * @property int                             $id
  * @property string|null                     $type
  * @property string|null                     $first_name
  * @property string|null                     $last_name
  * @property string|null                     $full_name
  * @property string|null                     $email
+<<<<<<< HEAD
+=======
+>>>>>>> fa4ebca (Lint)
+>>>>>>> f06764d (.)
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null                     $user_id
  * @property string|null                     $updated_by
  * @property string|null                     $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
+<<<<<<< HEAD
  * @property string|null                     $deleted_by
  * @property int                             $is_active
  *
  * @method static \Modules\Gdpr\Database\Factories\ProfileFactory factory($count = null, $state = [])
+=======
+<<<<<<< HEAD
+ * @property string|null $deleted_by
+ * @property int $is_active
+ *
+ * @method static \Modules\Gdpr\Database\Factories\ProfileFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereFullName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUserId($value)
+=======
+ * @property string|null                     $deleted_by
+ * @property int                             $is_active
+ *
+ * @method static \Modules\Gdpr\Database\Factories\ProfileFactory factory($count = null, $state = [])
+>>>>>>> f06764d (.)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile   newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Profile   newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Profile   query()
@@ -38,6 +83,10 @@ namespace Modules\Gdpr\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|Profile   whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile   whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile   whereUserId($value)
+<<<<<<< HEAD
+=======
+>>>>>>> fa4ebca (Lint)
+>>>>>>> f06764d (.)
  *
  * @mixin \Eloquent
  */
