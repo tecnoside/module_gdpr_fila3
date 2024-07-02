b51c068074a23d0640d9d51f3141a0f55409892f
marco <marco.sottana@gmail.com>
2024-07-02 17:08:37 +0200

    ♻️ (Event.php): Remove commented out code and unnecessary annotations to clean up the file and improve readability.

--- Models/Event.php
+++ Models/Event.php
@@ -13,39 +13,6 @@ use Illuminate\Support\Facades\Crypt;
 
 use function Safe\json_encode;
 
-<<<<<<< HEAD
-=======
-/**
- * Modules\Gdpr\Models\Event.
- *
- * @property string $id
- * @property string|null $treatment_id
- * @property string|null $consent_id
- * @property string $subject_id
- * @property string $ip
- * @property string $action
- * @property string $payload
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property Consent|null $consent
- *
- * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- * @method static Builder|Event newModelQuery()
- * @method static Builder|Event newQuery()
- * @method static Builder|Event query()
- * @method static Builder|Event whereAction($value)
- * @method static Builder|Event whereConsentId($value)
- * @method static Builder|Event whereCreatedAt($value)
- * @method static Builder|Event whereId($value)
- * @method static Builder|Event whereIp($value)
- * @method static Builder|Event wherePayload($value)
- * @method static Builder|Event whereSubjectId($value)
- * @method static Builder|Event whereTreatmentId($value)
- * @method static Builder|Event whereUpdatedAt($value)
- *
- * @mixin \Eloquent
- */
->>>>>>> a8c3108 (up)
 class Event extends BaseModel
 {
     use HasUuids;
