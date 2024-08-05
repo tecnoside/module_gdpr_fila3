b8dafa3c4ee0bff966a28efb2f8b413b0e649742
marco76tv <marco.sottana@gmail.com>
2024-08-05 16:47:55 +0200

    chore(Consent.php): update property indentation and alignment for better readability chore(Event.php): update property indentation and alignment for better readability
    refactor(Profile.php): improve code formatting and alignment for better readability
    refactor(Treatment.php): improve code formatting and alignment for better readability

--- Models/Consent.php
+++ Models/Consent.php
@@ -10,39 +10,36 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 /**
  * Modules\Gdpr\Models\Consent.
  *
- * @property string $id
- * @property string $treatment_id
- * @property string $subject_id
+ * @property string                          $id
+ * @property string                          $treatment_id
+ * @property string                          $subject_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property Treatment|null                  $treatment
+ *
+ * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereTreatmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
+ *
  * @property Treatment|null $treatment
  *
  * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent query()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereTreatmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
+ *                                                                                                    >>>>>>> fa4ebca (Lint)
  *
-<<<<<<< HEAD
- * @property Treatment|null $treatment
- *
- * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent query()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereUpdatedAt($value)
- *                                                                                     >>>>>>> fa4ebca (Lint)
- *
-=======
->>>>>>> b78960d (Dusting)
  * @mixin \Eloquent
  */
 class Consent extends BaseModel
--- Models/Treatment.php
+++ Models/Treatment.php
@@ -9,47 +9,44 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 /**
  * Modules\Gdpr\Models\Treatment.
  *
- * @property string $id
- * @property int $active
- * @property int $required
- * @property string $name
- * @property string $description
- * @property string|null $documentVersion
- * @property string|null $documentUrl
- * @property int $weight
+ * @property string                          $id
+ * @property int                             $active
+ * @property int                             $required
+ * @property string                          $name
+ * @property string                          $description
+ * @property string|null                     $documentVersion
+ * @property string|null                     $documentUrl
+ * @property int                             $weight
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  *
  * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
- * @method static Builder|Treatment newModelQuery()
- * @method static Builder|Treatment newQuery()
- * @method static Builder|Treatment query()
- * @method static Builder|Treatment whereActive($value)
- * @method static Builder|Treatment whereCreatedAt($value)
- * @method static Builder|Treatment whereDescription($value)
- * @method static Builder|Treatment whereDocumentUrl($value)
- * @method static Builder|Treatment whereDocumentVersion($value)
- * @method static Builder|Treatment whereId($value)
- * @method static Builder|Treatment whereName($value)
- * @method static Builder|Treatment whereRequired($value)
- * @method static Builder|Treatment whereUpdatedAt($value)
- * @method static Builder|Treatment whereWeight($value)
-<<<<<<< HEAD
- * @method static Builder|Treatment newModelQuery()
- * @method static Builder|Treatment newQuery()
- * @method static Builder|Treatment query()
- * @method static Builder|Treatment whereActive($value)
- * @method static Builder|Treatment whereCreatedAt($value)
- * @method static Builder|Treatment whereDescription($value)
- * @method static Builder|Treatment whereDocumentUrl($value)
- * @method static Builder|Treatment whereDocumentVersion($value)
- * @method static Builder|Treatment whereId($value)
- * @method static Builder|Treatment whereName($value)
- * @method static Builder|Treatment whereRequired($value)
- * @method static Builder|Treatment whereUpdatedAt($value)
- * @method static Builder|Treatment whereWeight($value)
-=======
->>>>>>> b78960d (Dusting)
+ * @method static Builder|Treatment                                 newModelQuery()
+ * @method static Builder|Treatment                                 newQuery()
+ * @method static Builder|Treatment                                 query()
+ * @method static Builder|Treatment                                 whereActive($value)
+ * @method static Builder|Treatment                                 whereCreatedAt($value)
+ * @method static Builder|Treatment                                 whereDescription($value)
+ * @method static Builder|Treatment                                 whereDocumentUrl($value)
+ * @method static Builder|Treatment                                 whereDocumentVersion($value)
+ * @method static Builder|Treatment                                 whereId($value)
+ * @method static Builder|Treatment                                 whereName($value)
+ * @method static Builder|Treatment                                 whereRequired($value)
+ * @method static Builder|Treatment                                 whereUpdatedAt($value)
+ * @method static Builder|Treatment                                 whereWeight($value)
+ * @method static Builder|Treatment                                 newModelQuery()
+ * @method static Builder|Treatment                                 newQuery()
+ * @method static Builder|Treatment                                 query()
+ * @method static Builder|Treatment                                 whereActive($value)
+ * @method static Builder|Treatment                                 whereCreatedAt($value)
+ * @method static Builder|Treatment                                 whereDescription($value)
+ * @method static Builder|Treatment                                 whereDocumentUrl($value)
+ * @method static Builder|Treatment                                 whereDocumentVersion($value)
+ * @method static Builder|Treatment                                 whereId($value)
+ * @method static Builder|Treatment                                 whereName($value)
+ * @method static Builder|Treatment                                 whereRequired($value)
+ * @method static Builder|Treatment                                 whereUpdatedAt($value)
+ * @method static Builder|Treatment                                 whereWeight($value)
  *
  * @mixin \Eloquent
  */
