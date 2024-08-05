9ede1bdae990da25925cc5a2d8fc49d1e9aa89d5
marco76tv <marco.sottana@gmail.com>
2024-08-05 16:49:42 +0200

    chore(Consent.php, Event.php, Profile.php, Treatment.php, EventServiceProvider.php): resolve linting conflicts and clean up code formatting

--- Models/Consent.php
+++ Models/Consent.php
@@ -16,6 +16,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property Treatment|null                  $treatment
+<<<<<<< HEAD
  *
  * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   newModelQuery()
@@ -28,6 +29,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
  *
  * @property Treatment|null $treatment
+=======
+>>>>>>> cbe9065 (Lint)
  *
  * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   newModelQuery()
@@ -38,7 +41,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereSubjectId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereTreatmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
+<<<<<<< HEAD
  *                                                                                                    >>>>>>> fa4ebca (Lint)
+=======
+>>>>>>> cbe9065 (Lint)
  *
  * @mixin \Eloquent
  */
