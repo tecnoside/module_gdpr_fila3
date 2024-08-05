52e13270e398adef2861ce4a9085af9d3df6116e
marco <marco.sottana@gmail.com>
2024-08-05 12:44:00 +0200

    up

--- Filament/Clusters/Profile/Resources/ProfileResource/Pages/ListProfiles.php
+++ Filament/Clusters/Profile/Resources/ProfileResource/Pages/ListProfiles.php
@@ -7,10 +7,21 @@ namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ProfileResource\Pages
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Modules\Gdpr\Filament\Clusters\Profile\Resources\ProfileResource;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 class ListProfiles extends ListRecords
 {
     protected static string $resource = ProfileResource::class;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
--- Filament/Resources/ConsentResource/Pages/ListConsents.php
+++ Filament/Resources/ConsentResource/Pages/ListConsents.php
@@ -7,10 +7,21 @@ namespace Modules\Gdpr\Filament\Resources\ConsentResource\Pages;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Modules\Gdpr\Filament\Resources\ConsentResource;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 class ListConsents extends ListRecords
 {
     protected static string $resource = ConsentResource::class;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
