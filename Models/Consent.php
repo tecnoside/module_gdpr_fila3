3e9ebd27b27c3e24cb9997a20b9401cfae1626a3
marco <marco.sottana@gmail.com>
2024-08-06 16:20:29 +0200

    📝 (Consent.php): Remove unnecessary comments and code from Consent model 📝 (Event.php): Remove unnecessary comments and code from Event model 📝 (Profile.php): Remove unnecessary comments and code from Profile model 📝 (Treatment.php): Remove unnecessary comments and code from Treatment model

--- Models/Consent.php
+++ Models/Consent.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
-<<<<<<< HEAD
  * @property string                          $id
  * @property string                          $treatment_id
  * @property string                          $subject_id
@@ -33,26 +32,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereTreatmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedBy($value)
-=======
- * Modules\Gdpr\Models\Consent.
- *
- * @property string $id
- * @property string $treatment_id
- * @property string $subject_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property Treatment|null $treatment
- *
- * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent query()
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Consent whereUpdatedAt($value)
->>>>>>> fb20669 (feat(Models): add new models Consent, Event, Profile, and Treatment to the GDPR module for managing consent, events, profiles, and treatments. Add necessary attributes, relationships, and encryption for sensitive data.)
  *
  * @mixin \Eloquent
  */
