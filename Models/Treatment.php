6cb2e3d7e3699182dc3319ba835876d19be38a28
marco <marco.sottana@gmail.com>
2024-08-06 16:31:49 +0200

    🔧 (Profile.php): Remove unnecessary code related to credits, slug, pivot, and membership properties 🔧 (Treatment.php): Remove unnecessary code related to various where methods and properties in the Treatment model

--- Models/Treatment.php
+++ Models/Treatment.php
@@ -23,7 +23,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @property string|null                     $deleted_by
  *
  * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
-<<<<<<< HEAD
  * @method static Builder|Treatment                                 newModelQuery()
  * @method static Builder|Treatment                                 newQuery()
  * @method static Builder|Treatment                                 query()
@@ -50,25 +49,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @method static Builder|Treatment                                 whereRequired($value)
  * @method static Builder|Treatment                                 whereUpdatedAt($value)
  * @method static Builder|Treatment                                 whereWeight($value)
-=======
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   query()
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereActive($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereDocumentUrl($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereDocumentVersion($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereRequired($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereWeight($value)
->>>>>>> ed9f0d6 (Lint)
  *
  * @mixin \Eloquent
  */
