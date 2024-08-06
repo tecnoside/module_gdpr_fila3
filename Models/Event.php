31581bd336d168628956984f601408b14125649e
marco <marco.sottana@gmail.com>
2024-08-06 16:22:17 +0200

    📝 (Consent.php, Event.php, Profile.php, Treatment.php): Resolve conflict and clean up code after linting process

--- Models/Event.php
+++ Models/Event.php
@@ -13,6 +13,58 @@ use Illuminate\Support\Facades\Crypt;
 
 use function Safe\json_encode;
 
+<<<<<<< HEAD
+=======
+/**
+ * Modules\Gdpr\Models\Event.
+ *
+ * @property string                          $id
+ * @property string|null                     $treatment_id
+ * @property string|null                     $consent_id
+ * @property string                          $subject_id
+ * @property string                          $ip
+ * @property string                          $action
+ * @property string                          $payload
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *                                                         <<<<<<< HEAD
+ * @property Consent|null                    $consent
+ *
+ * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+ * @method static Builder|Event                                 newModelQuery()
+ * @method static Builder|Event                                 newQuery()
+ * @method static Builder|Event                                 query()
+ * @method static Builder|Event                                 whereAction($value)
+ * @method static Builder|Event                                 whereConsentId($value)
+ * @method static Builder|Event                                 whereCreatedAt($value)
+ * @method static Builder|Event                                 whereId($value)
+ * @method static Builder|Event                                 whereIp($value)
+ * @method static Builder|Event                                 wherePayload($value)
+ * @method static Builder|Event                                 whereSubjectId($value)
+ * @method static Builder|Event                                 whereTreatmentId($value)
+ * @method static Builder|Event                                 whereUpdatedAt($value)
+ *                                                                                                  =======
+ *
+ * @property Consent|null $consent
+ *
+ * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
+ * @method static Builder|Event                                 newModelQuery()
+ * @method static Builder|Event                                 newQuery()
+ * @method static Builder|Event                                 query()
+ * @method static Builder|Event                                 whereAction($value)
+ * @method static Builder|Event                                 whereConsentId($value)
+ * @method static Builder|Event                                 whereCreatedAt($value)
+ * @method static Builder|Event                                 whereId($value)
+ * @method static Builder|Event                                 whereIp($value)
+ * @method static Builder|Event                                 wherePayload($value)
+ * @method static Builder|Event                                 whereSubjectId($value)
+ * @method static Builder|Event                                 whereTreatmentId($value)
+ * @method static Builder|Event                                 whereUpdatedAt($value)
+ *                                                                                                  >>>>>>> fa4ebca (Lint)
+ *
+ * @mixin \Eloquent
+ */
+>>>>>>> e13ef55 (Lint)
 class Event extends BaseModel
 {
     use HasUuids;
