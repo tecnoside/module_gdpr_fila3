b0f363eceb1f665be8b9d711912f81a959682933
marco <marco.sottana@gmail.com>
2024-08-06 16:25:46 +0200

    📝 (Event.php): resolve conflict in the Event model file and remove unnecessary conflict markers

--- Models/Event.php
+++ Models/Event.php
@@ -25,6 +25,11 @@ use function Safe\json_encode;
  * @property string                          $payload
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+ *                                                         <<<<EAD
+>>>>>>> 85df831 (docs(Event.php): add link to external documentation for reference)
  * @property Consent|null                    $consent
  *
  * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
@@ -41,6 +46,8 @@ use function Safe\json_encode;
  * @method static Builder|Event                                 whereTreatmentId($value)
  * @method static Builder|Event                                 whereUpdatedAt($value)
  *
+=======
+>>>>>>> 1927dd2 (conflict)
  * @property Consent|null $consent
  *
  * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
