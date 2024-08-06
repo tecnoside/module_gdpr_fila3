8631ef9c525eddeb0521bf984bba102ca5680e4e
marco <marco.sottana@gmail.com>
2024-08-06 16:20:29 +0200

    📝 (Consent.php): Remove unnecessary comments and code from Consent model 📝 (Event.php): Remove unnecessary comments and code from Event model 📝 (Profile.php): Remove unnecessary comments and code from Profile model 📝 (Treatment.php): Remove unnecessary comments and code from Treatment model

--- Models/Profile.php
+++ Models/Profile.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Models;
 
-<<<<<<< HEAD
 use Modules\User\Models\BaseProfile as UserBaseProfile;
 
 /**
@@ -72,48 +71,6 @@ use Modules\User\Models\BaseProfile as UserBaseProfile;
  * @mixin \Eloquent
  */
 class Profile extends UserBaseProfile
-=======
-// use Modules\User\Models\BaseProfile;
-
-/**
- * @property int $id
- * @property string|null $type
- * @property string|null $first_name
- * @property string|null $last_name
- * @property string|null $full_name
- * @property string|null $email
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $user_id
- * @property string|null $updated_by
- * @property string|null $created_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null $deleted_by
- * @property int $is_active
- *
- * @method static \Modules\Gdpr\Database\Factories\ProfileFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Profile newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Profile newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Profile query()
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereFirstName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereFullName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereIsActive($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereLastName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUserId($value)
- *
- * @mixin \Eloquent
- */
-class Profile extends BaseModel
->>>>>>> fb20669 (feat(Models): add new models Consent, Event, Profile, and Treatment to the GDPR module for managing consent, events, profiles, and treatments. Add necessary attributes, relationships, and encryption for sensitive data.)
 {
     /** @var string */
     protected $connection = 'gdpr';
--- Models/Treatment.php
+++ Models/Treatment.php
@@ -7,7 +7,6 @@ namespace Modules\Gdpr\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 /**
-<<<<<<< HEAD
  * @property string                          $id
  * @property int                             $active
  * @property int                             $required
@@ -41,35 +40,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   whereWeight($value)
-=======
- * Modules\Gdpr\Models\Treatment.
- *
- * @property string $id
- * @property int $active
- * @property int $required
- * @property string $name
- * @property string $description
- * @property string|null $documentVersion
- * @property string|null $documentUrl
- * @property int $weight
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- *
- * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
- * @method static Builder|Treatment newModelQuery()
- * @method static Builder|Treatment newQuery()
- * @method static Builder|Treatment query()
- * @method static Builder|Treatment whereActive($value)
- * @method static Builder|Treatment whereCreatedAt($value)
- * @method static Builder|Treatment whereDescription($value)
- * @method static Builder|Treatment whereDocumentUrl($value)
- * @method static Builder|Treatment whereDocumentVersion($value)
- * @method static Builder|Treatment whereId($value)
- * @method static Builder|Treatment whereName($value)
- * @method static Builder|Treatment whereRequired($value)
- * @method static Builder|Treatment whereUpdatedAt($value)
- * @method static Builder|Treatment whereWeight($value)
->>>>>>> fb20669 (feat(Models): add new models Consent, Event, Profile, and Treatment to the GDPR module for managing consent, events, profiles, and treatments. Add necessary attributes, relationships, and encryption for sensitive data.)
  *
  * @mixin \Eloquent
  */
