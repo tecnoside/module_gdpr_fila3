79971612a27bb06e37a345bd204e498817c7c166
marco <marco.sottana@gmail.com>
2024-08-06 16:29:17 +0200

    🔧 (Consent.php, Profile.php, Treatment.php): remove unnecessary properties and methods from models to clean up codebase and improve maintainability

--- Models/Treatment.php
+++ Models/Treatment.php
@@ -34,6 +34,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @method static Builder|Treatment                                 whereRequired($value)
  * @method static Builder|Treatment                                 whereUpdatedAt($value)
  * @method static Builder|Treatment                                 whereWeight($value)
+<<<<<<< HEAD
  * @method static Builder|Treatment                                 newModelQuery()
  * @method static Builder|Treatment                                 newQuery()
  * @method static Builder|Treatment                                 query()
@@ -47,6 +48,8 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
  * @method static Builder|Treatment                                 whereRequired($value)
  * @method static Builder|Treatment                                 whereUpdatedAt($value)
  * @method static Builder|Treatment                                 whereWeight($value)
+=======
+>>>>>>> 4f2d271 (chore(Consent.php, Event.php, Profile.php, Treatment.php): remove unnecessary)
  *
  * @mixin \Eloquent
  */
