5c85db82bbb8df57547c44dd967d0b3ddb8f1f76
marco76tv <marco76tv@users.noreply.github.com>
2024-09-09 13:19:11 +0000

    Lint

--- Models/BaseMorphPivot.php
+++ Models/BaseMorphPivot.php
@@ -50,7 +50,7 @@ abstract class BaseMorphPivot extends MorphPivot
 
     /** @var string */
     protected $primaryKey = 'id';
-/** @var string */
+    /** @var string */
     protected $keyType = 'string';
 
     /** @var list<string> */
@@ -76,8 +76,8 @@ abstract class BaseMorphPivot extends MorphPivot
     protected function casts(): array
     {
         return [
-                'id'=>'string',
-                'uuid'=>'string',
+            'id' => 'string',
+            'uuid' => 'string',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
