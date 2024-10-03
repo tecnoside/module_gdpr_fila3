a0df9f97ccbb9d71ef14d66a328e0d2b691a9138
marco <marco.sottana@gmail.com>
2024-10-03 17:12:25 +0200

    up

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -4,9 +4,23 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Providers\Filament;
 
+use Filament\Panel;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
 
 class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Gdpr';
+
+    public function panel(Panel $panel): Panel
+    {
+        FilamentAsset::register([
+            Css::make('gdpr-styles', asset('/vendor/cookie-consent/css/cookie-consent.css')),
+            // Js::make('gdpr-scripts', __DIR__.'/../../Resources/dist/assets/app2.js'),
+        ], 'gdpr');
+
+        return parent::panel($panel);
+    }
 }
