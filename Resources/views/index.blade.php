c9e3feaead16102dc68aa2b153c8882900a822f7
Malebestia <vair81@gmail.com>
2024-10-07 16:19:13 +0200

    rebase 3/57

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('gdpr::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('gdpr.name') !!}
+    </p>
+@endsection
