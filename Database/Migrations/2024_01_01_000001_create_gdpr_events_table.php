54e921d5ffef337c26658102d894dc01046d02dc
Malebestia <vair81@gmail.com>
2024-10-07 15:55:35 +0200

    rebase 2/22

--- Database/Migrations/2024_01_01_000001_create_gdpr_events_table.php
+++ Database/Migrations/2024_01_01_000001_create_gdpr_events_table.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Schema\Blueprint;
 use Modules\Gdpr\Models\Event;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
-return new class extends XotBaseMigration {
+return new class extends XotBaseMigration
+{
     protected ?string $model_class = Event::class;
 
     /**
