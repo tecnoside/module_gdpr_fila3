ccf8ad661a822e42cea484ed543bf9e92060c750
Malebestia <vair81@gmail.com>
2024-10-07 16:28:25 +0200

    rebase 5/231

---
+++ Filament/Pages/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Gdpr\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    // protected static string $view = 'gdpr::filament.pages.dashboard';
+}
---
+++ _docs/consents.txt
@@ -0,0 +1,10 @@
+https://packagist.org/packages/dialect/laravel-gdpr-compliance
+
+https://github.com/papertank/origami-consent
+
+https://github.com/foothing/laravel-gdpr-consent
+
+https://github.com/maize-tech/laravel-legal-consent
+
+
+
