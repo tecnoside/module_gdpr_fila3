ee733d034acb352976ad60fa5b731ded68b70d5a
marco <marco.sottana@gmail.com>
2024-10-29 17:34:07 +0100

    up

--- Models/BaseMorphPivot.php
+++ Models/BaseMorphPivot.php
@@ -74,7 +74,7 @@ abstract class BaseMorphPivot extends MorphPivot
     }
 
     /** @return array<string, string> */
-    public function casts(): array
+    protected function casts(): array
     {
         return [
             'id' => 'string',
