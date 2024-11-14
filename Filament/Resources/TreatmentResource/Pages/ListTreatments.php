f5390f1ad897bcbfe07bfd93186216defa3b610f
marco76tv <marco.sottana@gmail.com>
2024-11-14 17:53:45 +0100

    up

--- Filament/Resources/TreatmentResource/Pages/ListTreatments.php
+++ Filament/Resources/TreatmentResource/Pages/ListTreatments.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace Modules\Gdpr\Filament\Resources\TreatmentResource\Pages;
 
 use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
-use Modules\Gdpr\Filament\Resources\TreatmentResource;
 use Modules\UI\Enums\TableLayoutEnum;
+use Filament\Resources\Pages\ListRecords;
+use Modules\Xot\Filament\Pages\XotBaseListRecords;
+use Modules\Gdpr\Filament\Resources\TreatmentResource;
 use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
-class ListTreatments extends ListRecords
+class ListTreatments extends XotBaseListRecords
 {
-    public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
-
+    
     protected static string $resource = TreatmentResource::class;
 
     protected function getTableHeaderActions(): array
